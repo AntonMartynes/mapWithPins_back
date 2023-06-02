48b3cbfa151280f9c1ef8fc4f0f3170778ba8c42
Anton Martynes <martynes1997@gmail.com>
2023-06-02 21:17:57 +0300

    db migrate to heroku

--- src/db/db.js
+++ src/db/db.js
@@ -1,18 +1,24 @@
 import pkg from 'pg';
-const { Pool } = pkg;
+const { Client } = pkg;
 
-const pool = new Pool({
-  user: process.env.DB_USERNAME || "postgres",
-  host: process.env.DB_HOST || 'localhost',
-  database: process.env.DB_BDNAME || 'mape_task',
-  password: process.env.DB_PASSWORD || '123',
+const client = new Client({
+  connectionString: 'postgres://zfpvqiuoinhoyy:68d94a690217394f4376454c37b347b3d4aa2b0b9e4c4e43f9451cc7b5e72fbf@ec2-52-215-68-14.eu-west-1.compute.amazonaws.com:5432/d3dmmvu4ppv9k',
+  ssl: {
+    rejectUnauthorized: false
+  },
+  user: process.env.DB_USERNAME || "zfpvqiuoinhoyy",
+  host: process.env.DB_HOST || 'ec2-52-215-68-14.eu-west-1.compute.amazonaws.com',
+  database: process.env.DB_BDNAME || 'd3dmmvu4ppv9k',
+  password: process.env.DB_PASSWORD || '68d94a690217394f4376454c37b347b3d4aa2b0b9e4c4e43f9451cc7b5e72fbf',
   port: 5432,
 });
 
+client.connect();
+
 export const getPoints = (request, response) => {
-  pool.query('SELECT * FROM places ORDER BY id ASC', (error, results) => {
+  client.query('SELECT * FROM places ORDER BY id ASC', (error, results) => {
     if (error) {
-      throw error
+      throw error.message
     }
     response.status(200).json(results.rows)
   })
@@ -21,7 +27,7 @@ export const getPoints = (request, response) => {
 export const createPoint = (request, response) => {
   const { name, description, latitude, longitude } = request.body
 
-  pool.query('INSERT INTO places (name, description, latitude, longitude) VALUES ($1, $2, $3, $4) RETURNING *', [name, description, latitude, longitude], (error, results) => {
+  client.query('INSERT INTO places (name, description, latitude, longitude) VALUES ($1, $2, $3, $4) RETURNING *', [name, description, latitude, longitude], (error, results) => {
     if (error) {
       throw error
     }
@@ -32,7 +38,7 @@ export const createPoint = (request, response) => {
 export const deletePoint = (request, response) => {
   const id = parseInt(request.params.id)
 
-  pool.query('DELETE FROM places WHERE id = $1', [id], (error, results) => {
+  client.query('DELETE FROM places WHERE id = $1', [id], (error, results) => {
     if (error) {
       throw error
     }
